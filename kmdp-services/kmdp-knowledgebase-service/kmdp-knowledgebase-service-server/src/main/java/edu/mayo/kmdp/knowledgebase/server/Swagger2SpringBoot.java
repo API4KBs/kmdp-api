e5ca36da5bf70829bf098c03973323c37d693d45
Davide Sottara <dsotty@gmail.com>
2019-03-11 23:32:23 -0500

    Initial commit

---
+++ kmdp-services/kmdp-knowledgebase-service/kmdp-knowledgebase-service-server/src/main/java/edu/mayo/kmdp/knowledgebase/server/Swagger2SpringBoot.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright © 2018 Mayo Clinic (RSTKNOWLEDGEMGMT@mayo.edu)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.mayo.kmdp.knowledgebase.server;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.ExitCodeGenerator;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+@ComponentScan(basePackages = {"io.swagger", "edu.mayo.kmdp.knowledgebase.server", "edu.mayo.kmdp.knowledgebase.impl",
+    "${scan.packages}"})
+public class Swagger2SpringBoot extends SpringBootServletInitializer implements CommandLineRunner {
+
+  @Override
+  public void run(String... arg0) throws Exception {
+    if (arg0.length > 0 && arg0[0].equals("exitcode")) {
+      throw new ExitException();
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    new SpringApplication(Swagger2SpringBoot.class).run(args);
+  }
+
+  class ExitException extends RuntimeException implements ExitCodeGenerator {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public int getExitCode() {
+      return 10;
+    }
+
+  }
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(Swagger2SpringBoot.class);
+  }
+
+}
